a28c47b91a6ce33b09c67cc3e3c773773fe90dc3
L F <franco100lucas@gmail.com>
2024-01-09 18:50:59 -0300

    Utilizando polimorfismo e dunder methods para apresentação

--- Herança.py
+++ Herança.py
@@ -1,3 +1,7 @@
+# Se duas classes possuem o mesmo pai por herança
+# Pode-se utilizar polimosfismo para utilizar métodos iguais de forma diferente
+# E essa classe filha pode ser utilizada como se fosse a classe pai
+
 class Programa:
     def __init__(self, nome, ano):
         # Um único _ é um atributo protegido
@@ -21,6 +25,17 @@ class Programa:
     def nome(self, novo_nome):
         self._nome = novo_nome.title()
 
+    def __str__(self):
+        # __str__ é utilizado para exibir o objeto em forma de string
+        return f"Nome: {self._nome} - Ano: {self.ano} - {self._likes} Likes"
+    
+    def __repr__(self): # Representação do objeto
+        # repr() é utilizado para debugar o código, uma vez que o método __str__() é utilizado para exibir o objeto
+        # uma função para desenvolvedores
+        print(f"__repr dentro da classe {__class__.__name__} - exibindo dentro da classe {self._nome}")
+        print("Dentro do __repr__")
+        return f"Nome: {self._nome} - Ano: {self.ano} - {self._likes} Likes"
+
 
 # Para utilizar o construtor do método pai basta fazer o seguinte:
     # super().__init__(atributos do construtor do pai)
@@ -32,27 +47,30 @@ class Filme(Programa): # Herdando da classe Programa
         super().__init__(nome, ano) # Acessando o construtor da classe pai(que contém os atributos herdados)
         self.duracao = duracao
 
+    def __str__(self):
+        return f"Nome: {self._nome} - Ano: {self.ano} - Duração: {self.duracao} min - {self._likes} Likes"
+    # Sobrescrevendo o método __str__() da classe pai com sua própria assinatura
+
 
 class Serie(Programa):
-    def __init__(self, nome, ano, temporadas): # Added "duracao" argument
+    def __init__(self, nome, ano, temporadas): 
         super().__init__(nome, ano) # Acessando o construtor da classe pai(que contém os atributos herdados)
         self.temporadas = temporadas
 
+    def __str__(self):
+        return f"Nome: {self._nome} - Ano: {self.ano} - Temporadas: {self.temporadas} - {self._likes} Likes"
+    # Sobrescrevendo o método __str__() da classe pai com sua própria assinatura
+
 Vingadores = Filme("Vingadores - Ultimato", 2019, 180)
-
-RK = Serie("Rick and Morty", 2013, 7) # Added "duracao" argument
-
-print(f"Nome: {Vingadores.nome} - Ano: {Vingadores.ano} - Duração: {Vingadores.duracao} minutos - Likes: {Vingadores.likes}")
-print(f"Nome: {RK._nome} - Ano: {RK.ano} - Temporadas: {RK.temporadas} - Likes: {RK._likes}")
+RK = Serie("Rick and Morty", 2013, 7) 
 
 playlist = [Vingadores, RK]
 
 for programa in playlist:
-    detalhes = programa.duracao if hasattr(programa, "duracao") else programa.temporadas
-    # hasattr() verifica se o objeto possui o atributo passado como argumento
-    # hasattr(objeto, atributo)
-    print(f" {programa._nome} - {programa.ano} - {detalhes} - {programa._likes}")
+    print(programa) # Forma com polimorfismo
+    # O método __str__() é o mesmo para as duas classes, porém é executado de forma diferente
+    # Pois cada classe possui um método __str__() diferente
+    # Isso é polimorfismo
 
-# Se duas classes possuem o mesmo pai por herança
-# Pode-se utilizar polimosfismo para utilizar métodos iguais de forma diferente
-# E essa classe filha pode ser utilizada como se fosse a classe pai
+Vingadores.__repr__()
+repr(RK)
