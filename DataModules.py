3706a3bbb1e08e92ea2f6a2d66da73d74b21f697
L F <franco100lucas@gmail.com>
2024-01-12 18:44:34 -0300

    Apresentando com exemplos de Data Models com dunder methods

---
+++ DataModules.py
@@ -0,0 +1,41 @@
+
+"""
+Data Models são métodos especiais que podemos implementar em nossas classes para torná-las mais poderosas.
+
+Inicialização de objetos:
+__init__ é o método construtor de objetos
+
+Representação de objetos:
+__str__ é o método que retorna uma string que representa o objeto
+__repr__ é o método que retorna uma string que representa o objeto
+__format__ é o método que retorna uma string que representa o objeto
+__bytes__ é o método que retorna uma string que representa o objeto
+
+Métodos especiais:
+__del__ é o método que deleta a instância
+__eq__ é o método que compara a igualdade de instâncias
+__lt__ é o método que compara a menoridade de instâncias
+__gt__ é o método que compara a maioridade de instâncias
+__le__ é o método que compara a menoridade ou igualdade de instâncias
+__ge__ é o método que compara a maioridade ou igualdade de instâncias
+
+Container ou sequencia:
+__contains__ é o método que verifica se um item está contido na instância
+__iter__ é o método que permite ser iterável
+__len__ é o método que retorna o tamanho da instância
+__getitem__ é o método que retorna um item da instância
+__reversed__ é o método que permite ser iterável de trás para frente
+
+Numéricos:
+__add__ é o método que retorna a soma de instâncias
+__sub__ é o método que retorna a subtração de instâncias
+__mul__ é o método que retorna a multiplicação de instâncias
+__div__ é o método que retorna a divisão de instâncias
+__mod__ é o método que retorna o resto da divisão de instâncias
+__pow__ é o método que retorna a potência de instâncias
+__round__ é o método que retorna o arredondamento de instâncias
+__floordiv__ é o método que retorna a divisão inteira de instâncias
+__truediv__ é o método que retorna a divisão de instâncias
+
+
+"""
